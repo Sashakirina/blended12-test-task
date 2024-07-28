499cfa51b3770609eb4d2e245331c6c68459827a
Oleksandra <sashakirina@gmail.com>
2024-07-28 14:32:34 +0200

    persist redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
-import { Heading } from 'components';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Header } from './components';
+import Home from 'pages/Home';
+import Rates from 'pages/Rates';
+import { useEffect } from 'react';
+import { getUserInfo } from 'service/opencagedataApi';
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({ coords }) =>
+      getUserInfo(coords),
+    );
+  }, []);
+  return (
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="/rates" element={<Rates />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
+  );
 };
